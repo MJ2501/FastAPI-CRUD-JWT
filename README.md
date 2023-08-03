d0f3e5f8c9cf59cb74d9090db7f1360f1392390f
MJ2501 <80623169+MJ2501@users.noreply.github.com>
2023-08-03 21:47:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# Take-home-assignment-Software-Developer
+# READ ME
+
+## Choice of Framework
+
+The chosen framework for this project is FastAPI. FastAPI is a modern, fast, web framework for building APIs with Python. It is built on top of Starlette for handling asynchronous requests and Pydantic for data validation and serialization. Some reasons for choosing FastAPI are:
+
+* Performance: FastAPI is built on Starlette, which is an asynchronous framework. This allows for high-performance handling of requests and responses.
+* Type Annotations: FastAPI leverages Python type annotations and Pydantic models for automatic validation and serialization of data. This improves code readability and reduces the chance of bugs.
+* Interactive Documentation: FastAPI automatically generates interactive API documentation using Swagger UI. This makes it easy to explore and understand the API endpoints.
+* Ease of Use: FastAPI has a simple and intuitive API, making it easy for developers to create APIs quickly.
+
+## DB SCHEMA
+
+The database schema consists of two tables: users and data.
+
+users Table:
+* id: Primary key for the user.
+* username: Unique username for the user.
+* email: Unique email address of the user.
+* password: Hashed password of the user.
+* full_name: Full name of the user.
+* age: Age of the user.
+* gender: Gender of the user.
+
+
+data Table:
+* id: Primary key for the data.
+* key: Unique key for the data.
+* value: Value associated with the key.
+
+## Instructions to Run the Code
+To run the code, follow these steps:
+1. Install the required dependencies:
+```bash 
+pip install fastapi uvicorn sqlalchemy mysql-connector-python passlib pyjwt python-multipart
+```
+2. Create the MySQL database with the name my_database and update the MySQL credentials in the code if necessary.
+3. Run the FastAPI application using Uvicorn:
+```bash
+uvicorn app:app --reload
+```
+4. The application will be accessible at 'http://127.0.0.1:8000'.
+
+## Instructions to Set Up the Code
+To set up the code, follow these steps:
+1. Clone the repository or download the code.
+2. Install the required dependencies as mentioned in step 1 of "Instructions to Run the Code".
+3. Create the MySQL database with the name my_database and update the MySQL credentials in the code if necessary.
+4. Run the FastAPI application using Uvicorn as mentioned in step 3 of "Instructions to Run the Code".
+
+## Docker-Compose File
+```yaml
+version: "3"
+services:
+  fastapi_app:
+    build: .
+    ports:
+      - "8000:8000"
+    environment:
+      - MYSQL_USER=root
+      - MYSQL_PASSWORD=mohit0205
+      - MYSQL_HOST=db
+      - MYSQL_PORT=3306
+      - MYSQL_DB=my_database
+      - SECRET_KEY=your_secret_key
+      - ALGORITHM=HS256
+      - ACCESS_TOKEN_EXPIRE_MINUTES=60
+    depends_on:
+      - db
+
+  db:
+    image: mysql:latest
+    environment:
+      - MYSQL_ROOT_PASSWORD=mohit0205
+      - MYSQL_DATABASE=my_database
+    ports:
+      - "3306:3306"
+    volumes:
+      - mysql_data:/var/lib/mysql
+
+volumes:
+  mysql_data:
+```
+
+## Running with Docker-Compose
+1. Save the docker-compose file to your project directory.
+2. Open a terminal in the project directory and run the following command to start the application:
+```bash
+docker-compose up --build
+```
+3. The FastAPI application will be accessible at 'http://127.0.0.1:8000'.
+
+Now, the entire system (FastAPI and MySQL) will be up and running using Docker Compose. You can access the API and interact with the database using the provided endpoints.
+
+Please note that you may need to adjust the database configurations and other environment variables based on your requirements.
+
+
+
+
---
+++ app.py
@@ -0,0 +1,274 @@
+from fastapi import FastAPI, HTTPException, Request, Depends, Header
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.responses import JSONResponse
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel, Field, EmailStr
+import re
+from datetime import timedelta
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+
+app = FastAPI()
+
+# Security
+# security = HTTPBasic() # For just access token
+security = HTTPBearer()
+
+
+# Database connection
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "mohit0205"
+MYSQL_HOST = "localhost"  # Change this to your MySQL host if it's not on localhost
+MYSQL_PORT = "3306"  # Change this if your MySQL port is different
+MYSQL_DB = "my_database"
+
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Database model
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(50), unique=True, index=True)
+    password = Column(String(50))
+    full_name = Column(String(100))
+    age = Column(Integer)
+    gender = Column(String(10))
+
+# Data Model
+class Data(Base):
+    __tablename__ = "data"
+
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String(100), unique=True, index=True)
+    value = Column(String(100))
+
+# Create tables in the database
+Base.metadata.create_all(bind=engine)
+
+# Password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Generate password hash
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+# Verify password
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Pydantic model for request data
+class UserRegister(BaseModel):
+    username: str = Field(..., min_length=5, max_length=50)
+    email: EmailStr
+    password: str = Field(..., min_length=8, pattern="^[A-Za-z\d@$!%*?&]{8,}$")
+    full_name: str = Field(..., max_length=100)
+    age: int = Field(..., gt=0)
+    gender: str = Field(..., max_length=10)
+
+class TokenData(BaseModel):
+    username: str
+
+# Pydantic model for request data
+class DataStore(BaseModel):
+    key: str = Field(..., max_length=100)
+    value: str = Field(...)
+
+SECRET_KEY = "your_secret_key"  # Replace this with a secure secret key
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+# Error Response Model
+class ErrorResponse(BaseModel):
+    status: str = "error"
+    code: str
+    message: str
+
+# Success Response Model
+class SuccessResponse(BaseModel):
+    status: str = "success"
+    message: str
+    data: dict
+
+# For DataStore(key - value)
+# class SuccessResponse(BaseModel):
+#     status: str = "success"
+#     message: str
+
+# Token Response Model
+class TokenResponse(BaseModel):
+    status: str = "success"
+    message: str = "Access token generated successfully."
+    data: dict
+
+
+
+# Endpoint to register a new user
+@app.post("/api/register", response_model=SuccessResponse, responses={400: {"model": ErrorResponse}})
+def register_user(user_data: UserRegister):
+    if not all(user_data.dict().values()):
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="INVALID_REQUEST", message="Invalid request. Please provide all required fields: username, email, password, full_name."))
+    # Check if the username or email already exists
+    session = SessionLocal()
+    user = session.query(User).filter(User.username == user_data.username).first()
+    if user:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="USERNAME_EXISTS", message="The provided username is already taken. Please choose a different username."))
+    
+    user = session.query(User).filter(User.email == user_data.email).first()
+    if user:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="EMAIL_EXISTS", message="The provided email is already registered. Please use a different email address."))
+    
+    # Additional validations
+    if not re.match(r"^[A-Za-z\d@$!%*?&]{8,}$", user_data.password):
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="INVALID_PASSWORD", message="The provided password does not meet the requirements. Password must be at least 8 characters long and contain a mix of uppercase and lowercase letters, numbers, and special characters."))
+
+    if user_data.age <= 0:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="INVALID_AGE", message="Invalid age value. Age must be a positive integer."))
+    
+    if not user_data.gender:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="GENDER_REQUIRED", message="Gender field is required. Please specify the gender (e.g., male, female, non-binary)."))
+
+    # Create a new user record
+    new_user = User(**user_data.dict())
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    user_data_dict = user_data.dict()
+    user_data_dict["user_id"] = new_user.id
+    session.close()
+    return SuccessResponse(message="User successfully registered!", data=user_data_dict)
+
+# Endpoint to generate access token
+@app.post("/api/token", response_model=TokenResponse, responses={401: {"model": ErrorResponse}, 500: {"model": ErrorResponse}})
+def generate_token(credentials: HTTPBasicCredentials = Depends(security)):
+    if not credentials.username or not credentials.password:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="MISSING_FIELDS", message="Missing fields. Please provide both username and password."))
+    
+    session = SessionLocal()
+    user = session.query(User).filter(User.username == credentials.username).first()
+    if not user or not verify_password(credentials.password, user.password):
+        raise HTTPException(status_code=401, detail=ErrorResponse(code="INVALID_CREDENTIALS", message="Invalid credentials. The provided username or password is incorrect."))
+
+    token_data = TokenData(username=user.username)
+    access_token = create_access_token(token_data)
+    session.close()
+    return TokenResponse(data={"access_token": access_token, "expires_in": ACCESS_TOKEN_EXPIRE_MINUTES * 60})
+
+# Endpoint to store a key-value pair in the database
+@app.post("/api/data", response_model=SuccessResponse, responses={400: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 500: {"model": ErrorResponse}})
+def store_data(data: DataStore, authorization: HTTPAuthorizationCredentials = Depends(security)):
+    access_token = authorization.credentials
+    token_data = verify_access_token(access_token)
+    if not token_data:
+        raise HTTPException(status_code=401, detail=ErrorResponse(code="INVALID_TOKEN", message="Invalid access token provided"))
+
+    # Check for missing or invalid key
+    if not data.key or not data.key.strip():
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="INVALID_KEY", message="The provided key is not valid or missing."))
+
+    # Check for missing or invalid value
+    if not data.value or not data.value.strip():
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="INVALID_VALUE", message="The provided value is not valid or missing."))
+
+    session = SessionLocal()
+    # Check if the key already exists
+    existing_data = session.query(Data).filter(Data.key == data.key).first()
+    if existing_data:
+        raise HTTPException(status_code=400, detail=ErrorResponse(code="KEY_EXISTS", message="The provided key already exists in the database. To update an existing key, use the update API."))
+
+    # Store the key-value pair
+    new_data = Data(**data.dict())
+    session.add(new_data)
+    session.commit()
+    session.close()
+    return SuccessResponse(message="Data stored successfully.")
+
+# Endpoint to retrieve the value associated with a specific key
+@app.get("/api/data/{key}", response_model=SuccessResponse, responses={400: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 404: {"model": ErrorResponse}, 500: {"model": ErrorResponse}})
+def retrieve_data(key: str, authorization: HTTPAuthorizationCredentials = Depends(security)):
+    access_token = authorization.credentials
+    token_data = verify_access_token(access_token)
+    if not token_data:
+        raise HTTPException(status_code=401, detail=ErrorResponse(code="INVALID_TOKEN", message="Invalid access token provided"))
+
+    session = SessionLocal()
+    # Retrieve the value associated with the key
+    data = session.query(Data).filter(Data.key == key).first()
+    if not data:
+        raise HTTPException(status_code=404, detail=ErrorResponse(code="KEY_NOT_FOUND", message="The provided key does not exist in the database."))
+
+    session.close()
+    return SuccessResponse(data={"key": data.key, "value": data.value})
+
+# Endpoint to update the value associated with an existing key
+@app.put("/api/data/{key}", response_model=SuccessResponse, responses={400: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 404: {"model": ErrorResponse}, 500: {"model": ErrorResponse}})
+def update_data(key: str, updated_data: DataStore, authorization: HTTPAuthorizationCredentials = Depends(security)):
+    access_token = authorization.credentials
+    token_data = verify_access_token(access_token)
+    if not token_data:
+        raise HTTPException(status_code=401, detail=ErrorResponse(code="INVALID_TOKEN", message="Invalid access token provided"))
+
+    session = SessionLocal()
+    # Retrieve the data associated with the key
+    data = session.query(Data).filter(Data.key == key).first()
+    if not data:
+        raise HTTPException(status_code=404, detail=ErrorResponse(code="KEY_NOT_FOUND", message="The provided key does not exist in the database."))
+
+    # Update the value associated with the key
+    data.value = updated_data.value
+    session.commit()
+    session.close()
+    return SuccessResponse(message="Data updated successfully.")
+
+# Endpoint to delete a key-value pair from the database
+@app.delete("/api/data/{key}", response_model=SuccessResponse, responses={400: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 404: {"model": ErrorResponse}, 500: {"model": ErrorResponse}})
+def delete_data(key: str, authorization: HTTPAuthorizationCredentials = Depends(security)):
+    access_token = authorization.credentials
+    token_data = verify_access_token(access_token)
+    if not token_data:
+        raise HTTPException(status_code=401, detail=ErrorResponse(code="INVALID_TOKEN", message="Invalid access token provided"))
+
+    session = SessionLocal()
+    # Retrieve the data associated with the key
+    data = session.query(Data).filter(Data.key == key).first()
+    if not data:
+        raise HTTPException(status_code=404, detail=ErrorResponse(code="KEY_NOT_FOUND", message="The provided key does not exist in the database."))
+
+    # Delete the key-value pair from the database
+    session.delete(data)
+    session.commit()
+    session.close()
+    return SuccessResponse(message="Data deleted successfully.")
+
+# Create access token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Verify access token and get token data
+def verify_access_token(access_token: str):
+    try:
+        token_data = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        return token_data
+    except JWTError:
+        return None
+
+# Error handling for INTERNAL_SERVER_ERROR
+@app.exception_handler(Exception)
+async def internal_server_error_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content=ErrorResponse(code="INTERNAL_SERVER_ERROR", message="An internal server error occurred. Please try again later.")
+    )
+
